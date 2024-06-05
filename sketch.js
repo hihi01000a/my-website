794ad20590243e1a35c8791a8dd270f324bcf952
hihi01000a <hihi01000a@gmail.com>
2024-06-05 16:51:52 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,5 @@
 let img;
-let y = -100;
-let falling = false;
+let raindrops = [];
 
 function preload() {
     img = loadImage('image.png'); // 이미지 파일 경로를 맞춰주세요
@@ -13,17 +12,37 @@ function setup() {
 
 function draw() {
     background(0); // 배경을 검정색으로 설정
-    if (falling) {
-        y += 5; // 이미지가 떨어지는 속도
-    }
-    image(img, width / 2 - img.width / 2, y);
 
-    if (y > height) {
-        y = -100; // 다시 위로 초기화
-        falling = false;
+    for (let i = raindrops.length - 1; i >= 0; i--) {
+        raindrops[i].update();
+        raindrops[i].display();
+        
+        if (raindrops[i].y > height) {
+            raindrops.splice(i, 1); // 화면 아래로 사라진 이미지 제거
+        }
     }
 }
 
-function mouseMoved() {
-    falling = true; // 마우스를 움직이면 이미지가 떨어짐
+function mousePressed() {
+    let size = random(20, 100); // 이미지 크기 랜덤 설정
+    let raindrop = new Raindrop(mouseX, -size, size);
+    raindrops.push(raindrop); // 새로운 이미지 추가
+}
+
+class Raindrop {
+    constructor(x, y, size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.speed = random(2, 5);
+    }
+
+    update() {
+        this.y += this.speed;
+    }
+
+    display() {
+        imageMode(CENTER);
+        image(img, this.x, this.y, this.size, this.size);
+    }
 }
